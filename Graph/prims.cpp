6494f08bcfd15ae110b63976f388199be2e54f6e
silverfang04 <pahulkalra33viii@gmail.com>
2020-07-11 19:51:17 +0530

    Prims

--- Graph/prims.cpp
+++ Graph/prims.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 struct compare{
     bool operator()(pair<int,int> a,pair<int,int> b){
-        return a.second > b.second;
+        return a.second>b.second;
     }
 };
+
 int main(){
-    // Sample Input
+
     vector<vector<int> > v={
          {0, 1, 4  },
          {0, 7, 8  } ,
@@ -24,43 +25,39 @@ int main(){
          {6, 8, 6  } ,
          {7, 8, 7  }
     };
-    int n=9,source=0;
-    // Input End
-
-    vector< pair<int,int> > G[n];
+    int n=9,random_node=0;
+    vector<int> dist(n,INT_MAX);
+    vector<bool> mstSet(n,false);
     
+    vector<pair<int,int> > G[n];
+
     for(int i=0;i<v.size();i++){
         G[v[i][0]].push_back(make_pair(v[i][1],v[i][2]));
         G[v[i][1]].push_back(make_pair(v[i][0],v[i][2]));
     }
 
-    // for(int i=0;i<n;i++){
-    //     for(auto v:G[i]){
-    //         cout<<i<<v.first<<v.second<<endl;
-    //     }
-    // }
-
-    vector<int> dist(n,INT_MAX);
-
     priority_queue< pair<int,int> , vector<pair<int,int> > , compare > pq;
-    
-    pq.push(make_pair(source,0));
-    dist[source]=0;
 
+    dist[random_node]=0;
+    pq.push(make_pair(random_node,0));
+    
     while(!pq.empty()){
         int u=pq.top().first;
         pq.pop();
+        mstSet[u]=true;
+
         for(auto i:G[u]){
             int v=i.first;
-            int len=i.second;
-            if(dist[u] + len < dist[v]){
-                dist[v]=dist[u]+len;
+            int weight=i.second;
+            
+            if(dist[v] > weight && mstSet[v]==false){
+                dist[v] = weight;
                 pq.push(make_pair(v,dist[v]));
             }
         }
     }
 
-    for(int i=0;i<n;i++)    cout<<dist[i]<<endl;
+    for(int i=0;i<n;i++)    cout<<i<<" - > "<<dist[i]<<endl;
 
     return 0;
 }
